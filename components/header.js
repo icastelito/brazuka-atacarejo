d876f48d525fa1465df07f5646984898e4c1e212
Castelo <i.castelobp@gmail.com>
2023-01-23 18:09:19 -0300

    Commit final

--- components/header.js
+++ components/header.js
@@ -1,29 +1,108 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import styled from "styled-components";
 
+const StyledHeader = styled.header`
+  padding: 10px;
+  text-align: center;
+  font-family: "Roboto", sans-serif;
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledDiv = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin-bottom: 10px;
+`;
+
+const StyledH1 = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+  color: #fff;
+  text-align: center;
+  margin-bottom: 20px;
+  padding: 10px;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const StyledInput = styled.input`
+  width: 355px;
+  height: 30px;
+  border-radius: 5px;
+  border: none;
+  padding: 5px;
+  margin-right: 10px;
+`;
+
+const StyledButton = styled.button`
+  width: 125px;
+  height: 30px;
+  border-radius: 5px;
+  border: none;
+  background-color: #fff;
+  color: #000;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+const StyledImg = styled.img`
+  width: 255px;
+  height: 255px;
+`;
 
 const Header = ({ data, renderResults }) => {
-    const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
 
-    const handleSearchChange = (e) => {
-        setSearchValue(e.target.value.toLowerCase());
-    }
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value.toLowerCase());
+  };
 
-    const filteredData = data.filter(item => item.title.toLowerCase().includes(searchValue) || item.category.toLowerCase().includes(searchValue.toLowerCase() ));
+  const filteredData = data.filter(
+    (item) =>
+      item.title.toLowerCase().includes(searchValue) ||
+      item.category.toLowerCase().includes(searchValue.toLowerCase())
+  );
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        renderResults(filteredData);
-        setSearchValue('');
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    renderResults(filteredData);
+    setSearchValue("");
+  };
 
-    return (
-        <header>
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="search" value={searchValue} onChange={handleSearchChange} placeholder="Search" />
-                <button>Search</button>
-            </form>  
-        </header>
-    )
-}
+  return (
+    <>
+      <StyledHeader>
+        <StyledImg src="https://uploaddeimagens.com.br/images/004/309/727/original/Castelo_Create_a_logo_for_a_Brazilian_retail_store_using_the_co_01520fdf-dce1-4bca-be81-023be5c9e404.png?1674505199" />
+        <StyledDiv>
+        <StyledH1> BRAZUKA ATACAREJO </StyledH1>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput
+            type="text"
+            name="search"
+            value={searchValue}
+            onChange={handleSearchChange}
+            placeholder="Search"
+          />
+          <StyledButton>Search</StyledButton>
+        </StyledForm>
+        </StyledDiv>
+      </StyledHeader>
+    </>
+  );
+};
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,89 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import truncate from "truncate-html";
-import { StyledProducts, StyledCards, Image, Title, Description, Price, Rating, Category } from './StylizedProducts';
+import {
+  StyledProducts,
+  StyledCards,
+  Image,
+  Title,
+  Description,
+  Price,
+  Rating,
+  Category,
+} from "./StylizedProducts";
 import Header from "../components/header.js";
 import styled from "styled-components";
 
 const dollarPriceFormatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
+  style: "currency",
+  currency: "USD",
 });
 
 function Products() {
-    const [data, setData] = useState([]);
-    const [searchResults, setSearchResults] = useState([]);
-    useEffect(() => {
-        setSearchResults(data);
-    }, [data]);
+  const [data, setData] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  useEffect(() => {
+    setSearchResults(data);
+  }, [data]);
 
-    const renderSearchResults = (results) => {
-        setSearchResults(results);
-    };
+  const renderSearchResults = (results) => {
+    setSearchResults(results);
+  };
 
-    useEffect(() => {
-        axios
-            .get("https://fakestoreapi.com/products")
-            .then((response) => {
-                setData(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    const StyledBG = styled.div`
-  background-image: linear-gradient(to right, #00cc99, #006699);
-  width: 100%;
-  min-height: 1080px;
-`;
+  const StyledBG = styled.div`
+    background-image: linear-gradient(to right, #00cc99, #006699);
+    width: 100%;
+    min-height: 1080px;
+  `;
 
-    return (
-        <>
-            <StyledBG>
-                <Header data={data} renderResults={renderSearchResults} />
-                <StyledProducts>
-                    {searchResults.length > 0
-                        ? searchResults.map((item) => (
-                            <StyledCards key={item.id}>
-                                <Title>{truncate(item.title, 25)}</Title>
-                                <Image src={item.image} />
-                                <Description>{truncate(item.description, 100)}</Description>
-                                <Price>{dollarPriceFormatter.format(item.price)}</Price>
-                                <Rating>
-                                    <span role="img" aria-label="rating">
-                                        ⭐
-                                    </span>
-                                    {item.rating.rate}
-                                </Rating>
-                                <Category>{item.category}</Category>
-                            </StyledCards>
-                        ))
-                        : (
-                            <StyledCards>No results found. Please try again.</StyledCards>
-                        )
-                    }
-                </StyledProducts>
-            </StyledBG>
-        </>
-    );
+  return (
+    <>
+      <StyledBG>
+        <Header data={data} renderResults={renderSearchResults} />
+        <StyledProducts>
+          {searchResults.length > 0 ? (
+            searchResults.map((item) => (
+              <StyledCards key={item.id}>
+                <Title>{truncate(item.title, 25)}</Title>
+                <Image src={item.image} />
+                <Description>{truncate(item.description, 100)}</Description>
+                <Price>{dollarPriceFormatter.format(item.price)}</Price>
+                <Rating>
+                  <span role="img" aria-label="rating">
+                    ⭐
+                  </span>
+                  {item.rating.rate}
+                </Rating>
+                <Category>{item.category}</Category>
+              </StyledCards>
+            ))
+          ) : (
+            <StyledCards>
+              No results found. Please try again or or wait a few seconds, the
+              API response can sometimes take a while. In the meantime you can
+              visit my github to see more projects:{" "}
+              <a href="https://github.com/icastelito">
+                https://github.com/icastelito{" "}
+              </a>
+              .
+            </StyledCards>
+          )}
+        </StyledProducts>
+      </StyledBG>
+    </>
+  );
 }
 
 export default Products;
